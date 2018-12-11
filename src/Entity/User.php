01d3ebaf391e99a5e7c3e8aba5abb808996ef558
Jacikb <jacik@toya.net.pl>
2018-12-11 20:57:16 +0100

    First

---
+++ src/Entity/User.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * User: jacek
+ * Date: 08.12.18
+ * Time: 23:56
+ */
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    protected $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    protected $firstName;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+
+}
