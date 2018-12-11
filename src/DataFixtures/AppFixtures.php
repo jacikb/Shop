3d69c1e60448febde6f0b4ea8ae936b969d03785
Jacikb <jacik@toya.net.pl>
2018-12-11 22:25:17 +0100

    First.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,15 +8,27 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var \Faker\Factory
+     */
+    private $faker;
+
+    public function __construct()
+    {
+        $this->faker = \Faker\Factory::create();
+    }
+
     public function load(ObjectManager $manager)
     {
-         $product = new Product();
-         $product->setName('Telefon');
-         $product->setDescription('Opis telefonu');
-         $product->setCategory(1);
-         $product->setPrice(500);
-         $manager->persist($product);
-
+for($i=0;$i<100;$i++) {
+    $product = new Product();
+    $product->setStatus(1);
+    $product->setName($this->faker->realText(20));
+    $product->setDescription($this->faker->realText(500));
+    $product->setCategory(1);
+    $product->setPrice(500);
+    $manager->persist($product);
+}
         $manager->flush();
     }
 }
---
+++ src/Entity/Product.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $category;
+
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCategory(): ?int
+    {
+        return $this->category;
+    }
+
+    public function setCategory(int $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+}
