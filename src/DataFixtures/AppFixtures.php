d7e399821c815fee4090607f99812b990ba0e513
Jacikb <jacik@toya.net.pl>
2018-12-11 22:03:05 +0100

    First.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,12 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+         $product = new Product();
+         $product->setName('Telefon');
+         $product->setDescription('Opis telefonu');
+         $product->setCategory(1);
+         $product->setPrice(500);
+         $manager->persist($product);
 
         $manager->flush();
     }
